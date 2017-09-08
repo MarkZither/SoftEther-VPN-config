d585d6fb9a32b4e29f6c839c71c4884ad5a30965
MarkZither <mark.burton@zither-it.co.uk>
2017-09-08 20:38:49 +0200

    added eth device lookup and override option

--- vpnsetup/vpninstall.sh
+++ vpnsetup/vpninstall.sh
@@ -5,6 +5,7 @@
 # Simple script to setup a SoftEther VPN using L2TP
 # Feedback: n2rj@arrl.net
 
+: <<'END'
 #Download URL (from webpage)
 DOWNLOADURL="http://www.softether-download.com/files/softether/v4.22-9634-beta-2016.11.27-tree/Linux/SoftEther_VPN_Server/32bit_-_ARM_EABI/softether-vpnserver-v4.22-9634-beta-2016.11.27-linux-arm_eabi-32bit.tar.gz"
 
@@ -36,13 +37,24 @@ sudo update-rc.d vpnserver defaults
 
 #Start VPN server
 sudo /etc/init.d/vpnserver start
-
+END
 #VPN configuration
 HOSTNAME=`hostname`
+pushd .
+cd /sys/class/net
+DEFAULTETHDEVICE=`for f in e*; do [[ -e $f ]] || continue; echo $f ; done | head -1 `
+popd
+echo $DEFAULTETHDEVICE
 CONFIGBASEFILENAME=commands.in
 mkdir .vpnsetuptemp
 cp $CONFIGBASEFILENAME .vpnsetuptemp/
 CONFIG=.vpnsetuptemp/$CONFIGBASEFILENAME
+echo "Please enter your network interface name ($DEFAULTETHDEVICE): "
+read ethdevice
+if [ -z "$ethdevice" ]; then
+    ethdevice = $DEFAULTETHDEVICE
+    echo "Using $DEFAULTETHDEVICE"
+fi
 echo "Please enter your softether admin password: "
 read softadmin
 echo "Please enter your IPSEC Secret: "
@@ -54,6 +66,7 @@ read pass
 echo "Please enter a dynamic DNS hostname (your callsign is easiest):"
 read ddnshost
 
+sed -i "s/DEVICE_RP/$ethdevice/g" $CONFIG
 sed -i "s/ADMINPASSWORD/$softadmin/g" $CONFIG
 sed -i "s/USERNAME_RP/$username/g" $CONFIG
 sed -i "s/PASSWORD_RP/$pass/g" $CONFIG
@@ -61,7 +74,7 @@ sed -i "s/SECRET_RP/$secret/g" $CONFIG
 sed -i "s/DDNS_HOSTNAME/$ddnshost/g" $CONFIG
 
 
-sudo /usr/local/vpnserver/vpncmd localhost:443 /SERVER /IN:$CONFIG
+#sudo /usr/local/vpnserver/vpncmd localhost:443 /SERVER /IN:$CONFIG
 
 
 
@@ -73,8 +86,8 @@ echo "#####################ROUTER INSTRUCTIONS#####################"
 echo "1. Set up this Raspberry Pi as a static DHCP in your router."
 echo "   This ensures that the IP will remain the same."
 echo
-echo "Hardware address/MAC address:" `cat /sys/class/net/enxb827eb78c0ec/address`
-echo "IP address:"                   `ip addr show enxb827eb78c0ec | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6`
+echo "Hardware address/MAC address:" `cat /sys/class/net/$ethdevice/address`
+echo "IP address:"                   `ip addr show $ethdevice | grep "inet " | cut -d '/' -f1 | cut -d ' ' -f6`
 echo
 echo
 echo "2. Forward the following ports to the IP address in step 1:"
